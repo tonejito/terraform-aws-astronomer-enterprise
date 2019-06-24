959e65a394b575819b4a3214c9e507400259e9d6
Steven Miller <sjmiller609@gmail.com>
2019-06-24 12:43:44 +0000

    increment to aws module 1.0.4

--- terraform/enterprise/main.tf
+++ terraform/enterprise/main.tf
@@ -1,6 +1,6 @@
 module "aws" {
   source          = "astronomer/astronomer-aws/aws"
-  version         = "1.0.3"
+  version         = "1.0.4"
   # source          = "./terraform-aws-astronomer-aws"
   deployment_id   = var.deployment_id
   admin_email     = var.email
