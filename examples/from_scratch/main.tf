bec608b3892a8cf60ae99ee50580896e186150c4
Steven Miller <sjmiller609@gmail.com>
2019-07-07 23:39:13 +0000

    terraform fmt on the example

--- examples/from_scratch/main.tf
+++ examples/from_scratch/main.tf
@@ -1,9 +1,9 @@
 variable deployment_id {}
 
 module "astronomer_aws_from_scratch" {
-  source  = "../.."
-  deployment_id = var.deployment_id
-  email = "steven@astronomer.io"
+  source         = "../.."
+  deployment_id  = var.deployment_id
+  email          = "steven@astronomer.io"
   route53_domain = "astronomer-development.com"
   management_api = "public"
   tags = {
